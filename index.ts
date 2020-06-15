5707652e81a524731616db550648e58312f8a5ae
vincent varlet <vincent.varlet@adevinta.com>
2020-06-15 17:52:34 +0200

    using type annotations for function parameters

--- index.ts
+++ index.ts
@@ -12,5 +12,9 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1'
 axios.get(url).then((response) => {
     const todo = response.data as Todo
     const {id, title, completed} = todo
-    console.log(`The todo with id: ${id} has a title of: ${title} and is: ${completed}`)
+    logTodo(id,title,completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`The todo with id: ${id} has a title of: ${title} and is: ${completed}`)
+}
